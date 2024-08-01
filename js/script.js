eca0f1d1a8655ae1fa37aa10379e8327c0e75527
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-02 00:37:55 +0300

    hw_2 done

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+const username = prompt('Enter your username');
+alert(`Hello ${username}, how are you?`);
+'use strict';
+// task_1
+
+const name = 'Jonn';
+const admin = name;
+
+alert(admin);
+
+// task_2
+
+let name_of_planet = 'Earth';
+let user_name = 'Dima';
+
+// task_3
+
+let sum = 10 +'10';
+console.log(sum,`Тип данних: `+ typeof(sum));
