4998214848cd1f896e28869154f2fcbfc7501ffb
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-02 00:39:35 +0300

    hw_3 done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+const num_1 = Number(prompt('Enter first number'));
+const num_2 = Number(prompt('Enter second number'));
+
+
+console.log('num_1 + num_2 = ', num_1 + num_2);
+console.log('num_1 - num_2 = ', num_1 - num_2);
+console.log('num_1 * num_2 = ', num_1 * num_2);
+console.log('num_1 / num_2 = ', num_1 / num_2);
