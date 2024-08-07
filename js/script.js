39248879ac517245d38b0d9949d034e8627251e5
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-07 21:59:00 +0300

    hw_8 done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 "use strict";
-const num_1 = +prompt('Enter your number 1');
-const num_2 = +prompt('Enter your number 1');
-const num_3 = +prompt('Enter your number 1');
+const num1 = prompt('Enter your number 1');
+const num2 = prompt('Enter your number 2');
+const num3 = prompt('Enter your number 3');
+if (num1 ===null || num2===null || num3 === null ){
+    alert("You must enter a number");
+} else{
+        const parsedNum1 = parseFloat(num1)
+        const parsedNum2 = parseFloat(num2)
+        const parsedNum3 = parseFloat(num3)
+    if (isNaN(parsedNum1)|| isNaN(parsedNum2) || isNaN(parsedNum3)){
+        alert("Please enter a valid number");
+    }else {
+        const result = (parsedNum1 + parsedNum2 + parsedNum3)/3
+        alert('result: ' + result);
+    }
+}
+console.log(num1);
+console.log(num2);
+console.log(num3);
 
-const result  = ( num_1 + num_2 + num_3)/3;
-alert('Result: ' + result);
