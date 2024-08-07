d0c395f8bbab30d9faba66bb35ffa1a70f04384f
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-07 22:14:07 +0300

    hw_7 done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 'use strict';
-let hour = +prompt('Enter your hour');
-const result = hour * 60 * 60;
-alert(`В ${hour} часах ${result} cекунд `);
+let hour = prompt('Enter your hour');
+if(hour === null){
+    alert('Please enter a valid hour');
+}else{
+    if(hour === 0){
+        alert('Please enter a hour > 0');
+    }else{
+        const parseHour= parseInt(hour);
+        if (isNaN(parseHour)){
+            alert('Please enter a valid hour');
+        }else {
+            const result = parseHour * 60 * 60;
+            alert(`В ${parseHour} часах ${result} cекунд `);
+        }
+    }
+}
