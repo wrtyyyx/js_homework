d23808f42f44be1f807fd5f120105344e69606e6
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-12 20:41:52 +0300

    hw_7 done

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+//
+// if(numOrStr === null) {
+//     console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+
+const numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch ()
