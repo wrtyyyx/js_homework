25aec215314fbb57a0728ce8731f683fcfc1ea53
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-08-28 13:09:47 +0300

    HW_17 DONE + sasha

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 'use strict';
+
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 const removeElement = function (arr, item) {
   const index = arr.indexOf(item);
+
+  if (Number.isNaN(item)) {
+    return alert('sasha dast nogi!');
+  }
 
   if (!arr.includes(item)) {
     alert(`${item} is not in arr`);
@@ -12,5 +17,5 @@ const removeElement = function (arr, item) {
   return arr;
 };
 
-removeElement(array, 10);
+removeElement(array, +prompt('Enter a number'));
 console.log(array);
