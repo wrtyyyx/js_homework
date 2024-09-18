027a6c10dfcf061b00a4a043aa8dda9c215075a9
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-09-18 22:54:57 +0300

    hw_27 done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// task 1
+// const getFactorial = (num) => {
+//   return num != 1 ? num * getFactorial(num - 1) : 1;
+// };
+// console.log(getFactorial(3));
+
+// task 2
+// function pow(num, degree) {
+//   if (degree === 0) {
+//     return 1;
+//   }
+//   return num * pow(num, degree - 1);
+// }
+//
+// console.log(pow(3, 2));
+
+// task 3
+
+// function sum(a, b) {
+//   if (b === 0) return a;
+//
+//   return sum(a + 1, b - 1);
+// }
+//
+// console.log(sum(10, 2));
