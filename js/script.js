84ae90695c2ccd88db1011416c205146360c7052
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-09-20 19:44:39 +0300

    hw_26 done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 'use strict';
 
 // task 1
-// const getFactorial = (num) => {
-//   return num != 1 ? num * getFactorial(num - 1) : 1;
-// };
-// console.log(getFactorial(3));
+const getFactorial = (num) => (num !== 1 ? num * getFactorial(num - 1) : 1);
+console.log(getFactorial(3));
 
 // task 2
-// function pow(num, degree) {
-//   if (degree === 0) {
-//     return 1;
-//   }
-//   return num * pow(num, degree - 1);
-// }
-//
-// console.log(pow(3, 2));
+function pow(num, degree) {
+  if (degree === 0) {
+    return 1;
+  }
+  return num * pow(num, degree - 1);
+}
+
+console.log(pow(3, 2));
 
 // task 3
 
-// function sum(a, b) {
-//   if (b === 0) return a;
-//
-//   return sum(a + 1, b - 1);
-// }
-//
-// console.log(sum(10, 2));
+function sum(a, b) {
+  if (b === 0) return a;
+
+  return sum(a + 1, b - 1);
+}
+
+console.log(sum(10, 2));
