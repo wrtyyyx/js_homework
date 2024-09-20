3c9881667d77c358555818369ccf0b372e7d75d8
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-09-20 20:32:53 +0300

    hw_29 done

--- js/script.js
+++ js/script.js
@@ -6,15 +6,14 @@ const complexArray = [
   [[10, 11], 12, 13],
   [33, 23, [23]],
 ];
-
-const flat = function (arr, depth = 0) {
-  let result = [];
+const flat = function (arr, result = []) {
   arr.forEach((item) => {
-    if (Array.isArray(item) && depth > 0) {
-      result = result.concat(flat(item, depth - 1));
-    } else result.push(item);
+    if (Array.isArray(item)) {
+      return flat(item, result);
+    }
+    result.push(item);
   });
   return result;
 };
 
-console.log(flat(complexArray, 4));
+console.log(flat(complexArray));
