b5a397d6db519ae2cbac521277a49c014045111b
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-09-21 16:50:44 +0300

    hw_28 done

--- js/script.js
+++ js/script.js
@@ -5,21 +5,11 @@ const getFactorial = (num) => (num !== 1 ? num * getFactorial(num - 1) : 1);
 console.log(getFactorial(3));
 
 // task 2
-function pow(num, degree) {
-  if (degree === 0) {
-    return 1;
-  }
-  return num * pow(num, degree - 1);
-}
+const pow = (num, degree) => (degree !== 0 ? num * pow(num, degree - 1) : 1);
 
 console.log(pow(3, 2));
 
 // task 3
+const sum = (a, b) => (b !== 0 ? sum(a + 1, b - 1) : a);
 
-function sum(a, b) {
-  if (b === 0) return a;
-
-  return sum(a + 1, b - 1);
-}
-
-console.log(sum(10, 2));
+console.log(sum(10, 24));
