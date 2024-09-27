b80db772b03b7998a0d3e3a1c75a8ed740e64571
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-09-27 19:59:56 +0300

    hw_32 done

--- js/script.js
+++ js/script.js
@@ -6,5 +6,6 @@ console.log(docBody);
 const imgRandom = Math.floor(Math.random() * 9) + 1;
 console.log(imgRandom);
 
-const img = document.getElementById('img');
+const img = document.createElement('img');
 img.src = `img/${imgRandom}.jpg`;
+document.body.append(img);
