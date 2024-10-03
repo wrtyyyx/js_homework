3044b269e6e75aeedbf13e8744f9a7d3f881cd54
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-10-03 18:11:04 +0300

    hw_32 update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
 'use strict';
 
-// const colum = 10;
-// const row = 10;
-// let num = 1;
-//
-// const createTable = (colum = 2; row = 2) => {
-//   const table = document.createElement('table');
-//   for (let i = 0; i < row; i++) {
-//     const rowElement = document.createElement('tr');
-//     for (let j = 0; j < colum; j++) {
-//       const td = document.createElement('td');
-//       rowElement.appendChild(td);
-//       td.innerHTML = num;
-//       num++;
-//     }
-//     table.appendChild(rowElement);
-//   }
-//   document.body.appendChild(table);
-// };
-//
-// createTable();
-
-// const generateTable = () => {
-//   const tabel = document.createElement('table');
-//   return tabel;
-
-// };
-
 const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
   if (!parentEl || !elementToAppend) return;
   parentEl.append(elementToAppend);
