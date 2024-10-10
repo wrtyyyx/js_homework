16b270977a4a83e1213f096b6ea314a840e9bb3b
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-10-10 19:28:18 +0300

    hw_35 done

--- js/solution.js
+++ js/solution.js
@@ -7,36 +7,48 @@ const firstLi = document.querySelector('li:first-child');
 console.log(ul.attributes);
 
 // task 1
+
 li.forEach((item) => {
   console.log(item.innerHTML);
 });
 
 // task 2
 
-const dataAttribute = [];
+const dataGetAtribute = () => {
+  const dataAttribute = [];
 
-for (const attr of ul.getAttributeNames()) {
-  dataAttribute.push(ul.getAttribute(attr));
-}
-console.log(dataAttribute);
+  for (const attr of ul.getAttributeNames()) {
+    dataAttribute.push(ul.getAttribute(attr));
+  }
+  console.log(dataAttribute);
+};
+dataGetAtribute();
 
 // task 3
+const dataGetAttNames = () => {
+  const dataNamesAttribute = [];
+  for (const attName of ul.getAttributeNames()) {
+    dataNamesAttribute.push(attName);
+  }
+  console.log(dataNamesAttribute);
+};
 
-const dataNamesAttribute = [];
-for (const attName of ul.getAttributeNames()) {
-  dataNamesAttribute.push(attName);
-}
-console.log(dataNamesAttribute);
+dataGetAttNames();
 
 // task 4
 
-lastLi.innerHTML = 'Привіт! Мене звуть Дмитро';
-console.log(lastLi.innerHTML);
+const changeLastItem = () => {
+  lastLi.innerHTML = 'Привіт! Мене звуть Дмитро';
+  console.log(lastLi.innerHTML);
+};
+changeLastItem();
 
 // task 5
-
-firstLi.setAttribute('data-my-name', 'Dmytro');
-console.log(firstLi);
+const changeFirstLi = () => {
+  firstLi.setAttribute('data-my-name', 'Dmytro');
+  console.log(firstLi);
+};
+changeFirstLi();
 
 // task 6
 ul.removeAttribute('data-dog-tail');
