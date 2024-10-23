8e8f9360c8d455cb1cd30a7710e083ffd0e46678
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2024-10-23 13:33:35 +0300

    hw_41 done

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const product = {
+  quantity: 12,
+};
+// name
+
+Object.defineProperty(product, 'name', {
+  value: 'Iphone',
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+if (typeof product.name !== 'string' || product.name.trim().length === 0) {
+  Object.defineProperty(product, 'name', {
+    writable: false,
+  });
+  throw new Error('Value of property name must be a string');
+}
+// price
+Object.defineProperty(product, 'price', {
+  value: 49990,
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+
+if (typeof product.price !== 'number' || product.price < 0) {
+  Object.defineProperty(product, 'price', {
+    writable: false,
+  });
+  throw new Error('Value of property price must be more than 0');
+}
+// totalValue
+Object.defineProperty(product, 'totalValue', {
+  get() {
+    return `TOTAL = ${this.price * this.quantity}`;
+  },
+});
+console.log(product);
+console.log(product.totalValue);
